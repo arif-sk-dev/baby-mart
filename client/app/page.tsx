635891c24b386a3fadda8a0d93d5945d988dcea2
arif-hossain <arifsk.dev@gmail.com>
2026-01-29 00:25:20 +0600

    client

---
+++ client/app/page.tsx
@@ -0,0 +1,14 @@
+// import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h2>Baby mart E-commerce</h2>
+      <Button>Hello!</Button>
+    </div>
+  );
+}
+
+  
