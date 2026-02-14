e0fc47e6e43a876a78f512b9e43dd904640dc9e4
arifstk <arifsk.dev@gmail.com>
2026-02-14 16:46:30 +0600

    add user modify

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,7 +10,7 @@ const getUsers = asyncHandler(async (req, res) => {
 export { getUsers };
 
 
-// CREATE user (ADMIN)
+// Create user (ADMIN)
 export const createUser = asyncHandler(async (req, res) => {
   const { name, email, password, role } = req.body;
 
@@ -21,6 +21,7 @@ export const createUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
+  // if user not exist (create new user)
   const user = await User.create({
     name,
     email,
@@ -29,5 +30,19 @@ export const createUser = asyncHandler(async (req, res) => {
     avatar: req.file ? req.file.path : "",
   });
 
-  res.status(201).json(user);
+  // res.status(201).json(user);
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      role: user.role,
+      addresses: user.addresses,
+    })
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
+
